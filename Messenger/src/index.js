aee81a56d39f72e6d8fbc74179f546be3bc5a15b
Hardik Singh <hardiksingh297@gmail.com>
2020-10-19 21:19:32 +0530

    Basic version finished

--- Messenger/src/index.js
+++ Messenger/src/index.js
@@ -1,1 +1,14 @@
-console.log("working");
+const express = require("express");
+const app = express();
+
+const router = require("./routes/router");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(router);
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
