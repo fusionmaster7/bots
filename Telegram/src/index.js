268d3d2e8668efdd476650424eff98e59d7abc7e
adiityax <64186221+adiityax@users.noreply.github.com>
2020-10-20 00:10:10 +0530

    basic setup extended

--- Telegram/src/index.js
+++ Telegram/src/index.js
@@ -1,1 +1,11 @@
-console.log("working")
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var axios = require('axios')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+) // for parsing application/x-www-form-urlencoded
