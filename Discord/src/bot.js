a2bc8f44913ae5022b60a84ec98e36833428ff55
Hardik Singh <hardiksingh297@gmail.com>
2020-10-24 11:44:43 +0530

    Basic Bot Response Setup

---
+++ Discord/src/bot.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const { messageController } = require("./controllers/response.controllers");
+const startBot = () => {
+  const client = new Discord.Client();
+
+  client.once("ready", () => {
+    console.log("Bot Ready");
+  });
+
+  client.on("message", (message) => {
+    const messageString = message.content;
+    const messageResponse = messageController(messageString, config.prefix);
+    if (messageResponse !== null) {
+      message.channel.send(messageResponse);
+    }
+  });
+
+  client.login(config.token);
+};
+
+module.exports = { startBot };
