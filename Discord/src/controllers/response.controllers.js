c417a9846dbbdf4504a0a383bb49396436867dab
Hardik Singh <hardiksingh297@gmail.com>
2020-10-25 12:34:41 +0530

    Fetch dialogflow response

--- Discord/src/controllers/response.controllers.js
+++ Discord/src/controllers/response.controllers.js
@@ -1,6 +1,11 @@
-const messageController = (messageString, commandPrefix) => {
+const { getResponseText } = require("./dialog.controllers");
+
+const messageController = async (messageString, commandPrefix) => {
   if (messageString.startsWith(commandPrefix)) {
-    return messageString.slice(1).trim();
+    const query = messageString.slice(1).trim();
+    const messageResponse = await getResponseText(query);
+    console.log(messageResponse);
+    return messageResponse;
   } else {
     return null;
   }
